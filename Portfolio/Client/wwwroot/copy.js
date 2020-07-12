232a8c8058e46ed4ef19946d7def91199bc5ac38
Jakob Horvath <horvath.j17@gmail.com>
2020-07-12 16:34:19 -0700

    First commit - base setup of website

---
+++ Portfolio/Client/wwwroot/copy.js
@@ -0,0 +1,10 @@
+﻿window.clipboardCopy = {
+    copyEmail: function(text) {
+        navigator.clipboard.writeText(text).then(function () {
+            alert("Email copied to clipboard!");
+        })
+            .catch(function (error) {
+                alert(error);
+            })
+    }
+};
